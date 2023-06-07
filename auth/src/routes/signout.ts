b2c22c268159ee90bd001a398a4e4a401a70b6c1
yincekara <yincekara@gmail.com>
2023-06-07 17:07:49 +0300

    Ticketing test

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,7 +2,7 @@ import express = require('express');
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
+router.post('/api/users/signout', (req, res) => {
     res.send('Hi there signout on local');
 });
 
