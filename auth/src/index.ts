32303b52e66cdfc8bda2e8c48266e72556f332a1
yincekara <yincekara@gmail.com>
2023-07-02 12:58:08 +0300

    User.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express = require('express');
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 
 import {currentUserRouter} from './routes/currentuser';
 import {signinRouter} from './routes/signin';
@@ -23,6 +24,20 @@ app.all('*', async (req, res) => {
 });
 app.use(errorhandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
+            //useCreateIndex: true
+        });
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!!!!!');
+    });
+};
+
+start();
---
+++ auth/src/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+//An interface that describe the properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+//An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+const buildUser = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+export {User};
