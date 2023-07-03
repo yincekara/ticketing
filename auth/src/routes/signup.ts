629551fbf5628b888a89126b371aade8e3a11809
yincekara <yincekara@gmail.com>
2023-07-03 12:00:17 +0300

    User.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express = require('express');
 
 import {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+mport {User} from "../models/user";
 import {RequestValidationError} from "../errors/requestvalidationerror";
 import {DatabaseConnectionError} from "../errors/databaseconnectionerror";
 
@@ -23,11 +24,24 @@ router.post('/api/users/signup', [
             //throw new Error('Invalid email or password');
             //return res.status(400).send(errors.array());
         }
+
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            console.log('email in use');
+            return res.send({});
+        }
+
+        const user = User.build({email, password});
+        await user.save();
+        res.status(201).send(user);
+        /*
         console.log('Creating a user');
         throw new DatabaseConnectionError();
         //throw new Error('Error connecting to database');
-
         res.send({});
+        */
+
     });
 
 export {router as signupRouter};
