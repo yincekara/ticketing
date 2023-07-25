aaa38551f6e82b2a2313fa6c544c1640879fac2a
yincekara <yincekara@gmail.com>
2023-07-25 15:57:35 +0300

    Signin Done

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express = require('express');
 import {Request, Response} from 'express';
 import {body} from 'express-validator';
+import {Password} from "../services/password";
+import {User} from '../models/user';
 import {validateRequest} from "../middlewares/validaterequests";
+import {BadRequestError} from "../errors/badrequesterror";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -16,8 +20,30 @@ router.post('/api/users/signin',
             .withMessage('You must supply a password')
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+        const passwordMatch = await Password.compare(existingUser.password, password);
+        if (!passwordMatch){
+            throw new BadRequestError('Invalid credentials');
+        }
+        // Generate JWT
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email
+            },
+            process.env.JWT_KEY!
+        );
 
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+        res.status(200).send(existingUser);
     });
 
 export {router as signinRouter};
