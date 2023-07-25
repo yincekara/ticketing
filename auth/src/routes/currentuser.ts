a525a21050918b6a2a6b170ea1cab6f87b15af74
yincekara <yincekara@gmail.com>
2023-07-25 17:24:02 +0300

    CurrentUser Done

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,9 +1,20 @@
 import express = require('express');
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there currentuser on local');
+    //res.send('Hi there currentuser on local');
+    if (!req.session?.jwt) {
+        return res.send({currentUser: null});
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send({currentUser: payload});
+    } catch (err) {
+        res.send({currentUser: null});
+    }
 });
 
 export {router as currentUserRouter};
