9ba24696a2c0b752733bf09ca71c19379c0870ab
yincekara <yincekara@gmail.com>
2023-07-26 10:57:10 +0300

    CurrentUser Middleware Done

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express = require('express');
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there signout on local');
+    req.session = null;
+    res.send({});
 });
 
 export {router as signoutRouter};
