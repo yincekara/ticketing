bdc87a7bb6a512fd50cc019ae9d0c67342135f06
yincekara <yincekara@gmail.com>
2023-09-08 11:56:45 +0300

    signup form development with error handling

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,23 @@
 import {useState} from 'react';
+import axios from 'axios';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const onSubmit = (event) => {
+    const [errors, setErrors] = useState([]);
+
+    const onSubmit = async event => {
         event.preventDefault();
-        console.log(email, password);
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email,
+                password,
+            });
+            console.log(response.data);
+        } catch (err) {
+            //console.log(err.response.data);
+            setErrors(err.response.data.errors);
+        }
     };
     return <form onSubmit={onSubmit}>
         <h1>Sign Up</h1>
@@ -18,6 +30,13 @@ export default () => {
             <input value={password} onChange={e => setPassword(e.target.value)} type="password"
                    className="form-control"/>
         </div>
+        {errors.length > 0 &&
+            <div className="alert alert-danger">
+                <h4>Something went wrong</h4>
+                <ul className="my-0">
+                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+            </div>}
         <button className="btn btn-primary">Sign Up</button>
     </form>
 }
