751602f37bd7218b99936ac1d61ef09761638879
yincekara <yincekara@gmail.com>
2023-12-06 14:54:24 +0300

    userequest development

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,17 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    axios.get('/api/users/currentuser').catch((err) => {
+        console.log(err.message);
+    });
+    console.log('currentUser: ', currentUser);
     return <h1>Landing Page</h1>;
 }
+/*
+LandingPage.getInitialProps = async () => {
+    console.log('I am on the server');
+    const response = await axios.get('api/users/currentuser');
+    return response.data;
+}
+*/
+export default LandingPage;
